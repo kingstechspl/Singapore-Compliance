2a2cd793179939717deae586d10878a0e0766bb6
viralkansodiya <viral@fosserp.com>
2026-04-02 10:07:45 +0530

    feat: implement PDF generation via XMLHttpRequest with error handling

--- singapore_compliance/public/js/process_statement_of_accounts.js
+++ singapore_compliance/public/js/process_statement_of_accounts.js
@@ -10,7 +10,45 @@ frappe.ui.form.on("Process Statement Of Accounts", {
 					console.log(r.message)
 					let p_html = set_html(frm, r.message);
 					console.log(p_html)
-					frappe.render_pdf(p_html, { orientation: "Portrait" });
+
+					var formData = new FormData();
+					formData.append("html", p_html);
+					formData.append("orientation", "Portrait");
+					formData.append("blob", new Blob([], { type: "text/xml" }));
+
+					var xhr = new XMLHttpRequest();
+					xhr.open("POST", "/api/method/frappe.utils.print_format.report_to_pdf");
+					xhr.setRequestHeader("X-Frappe-CSRF-Token", frappe.csrf_token);
+					xhr.responseType = "arraybuffer";
+
+					xhr.onload = function () {
+						if (this.status === 200) {
+							var blob = new Blob([this.response], { type: "application/pdf" });
+							var objectUrl = URL.createObjectURL(blob);
+							let a = document.createElement("a");
+							document.body.appendChild(a);
+							a.style = "display: none";
+							a.href = objectUrl;
+							a.download = frm.doc.name + ".pdf";
+							a.click();
+							window.URL.revokeObjectURL(objectUrl);
+						} else {
+							var errorText = new TextDecoder().decode(this.response);
+							frappe.msgprint({
+								title: __("PDF Generation Failed"),
+								message: __("Server error {0}: {1}", [this.status, errorText]),
+								indicator: "red",
+							});
+						}
+					};
+					xhr.onerror = function () {
+						frappe.msgprint({
+							title: __("PDF Generation Failed"),
+							message: __("Network error while generating PDF."),
+							indicator: "red",
+						});
+					};
+					xhr.send(formData);
 				},
 			});
 		});
